7042c310492831b7e8b2615954f11751dc4f5e2b
Paras Gupta <88033881+ParasGupta-BCA@users.noreply.github.com>
2025-05-29 16:03:19 +0530

    app-sidebar.tsx

--- indicachat/components/app-sidebar.tsx
+++ indicachat/components/app-sidebar.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
-import { Github, Globe } from "lucide-react"
+import { Globe } from "lucide-react"
 import Link from "next/link"
-import Image from "next/image"
 import {
   Sidebar,
   SidebarContent,
